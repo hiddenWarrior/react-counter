fcde1ed8fdf27cc4631fc301f5da277cb71ad227
hiddenwarrior <manfscience@gmail.com>
2018-12-18 10:37:07 +0200

    multtiple counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0, tags: ["tag1", "tag2", "tag3"] };
+  state = { value: this.props.value, tags: ["tag1", "tag2", "tag3"] };
 
   //   constructor() {
   //     super();
   //     this.handleIncrement = this.handleIncrement.bind(this);
   //   }
-  handleIncrement = () => this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => this.setState({ value: this.state.value + 1 });
 
   render() {
     return (
-      <React.Fragment>
+      <div>
+        {this.props.children}
         {/* {this.renderTags()} */}
         {/* <img src={this.state.imageUrl} alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -21,7 +22,7 @@ class Counter extends Component {
         >
           increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
@@ -41,13 +42,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
